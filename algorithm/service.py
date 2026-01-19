0751ec58f9d73d448dc36af9d30d398785da0c95
surface <2189453698@qq.com>
2026-01-19 09:44:52 +0800

    修改:因果分析

--- algorithm/service.py
+++ algorithm/service.py
@@ -1362,6 +1362,7 @@ class AlgorithmIntegrationService(IAlgorithmIntegrationService):
                     retryable_exceptions=(ConnectionError, TimeoutError, asyncio.TimeoutError),
                     context={'operation': 'association_execution', 'algorithm_type': algorithm_type.value}
                 )
+            
             elif algorithm_type == AlgorithmType.SIMILARITY:
                 return await self.retry_handler.retry_async(
                     self.algorithm_executor.execute_similarity,
